b9a491478a02cbc78866e0e8a307d53a8ecf9290
ThomasDev82 <82886071+ThomasDev82@users.noreply.github.com>
2021-11-06 02:12:23 +0200

    Add files via upload

---
+++ login_screen/lib/login_screen.dart
@@ -0,0 +1,99 @@
+// ignore_for_file: avoid_print, avoid_unnecessary_containers, sized_box_for_whitespace, must_be_immutable
+
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+class LoginScreen extends StatelessWidget
+{
+  var emailControler = TextEditingController();
+  var passwordControler = TextEditingController();
+  LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children:
+            [
+              const SizedBox(height: 20),
+              const Text(
+                'Login',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 40),
+              TextFormField(
+                controller: emailControler,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  labelText: 'Email Address',
+                  prefixIcon: Icon(Icons.alternate_email_outlined),
+                  hintText: 'Type your email address',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: passwordControler,
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  prefixIcon: Icon(Icons.password_outlined),
+                  suffixIcon: Icon(Icons.remove_red_eye_outlined),
+                  hintText: 'Type your password',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 35),
+              Container(
+                width: double.infinity,
+                color: Colors.blue,
+                child: MaterialButton(
+                  onPressed: ()
+                  {
+                    print(emailControler.text);
+                    print(passwordControler.text);
+                  },
+                  child: const Text(
+                    'LOGIN',
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children:
+                [
+                  const Text(
+                    'Don\'t have an account?',
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Register Here',
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
