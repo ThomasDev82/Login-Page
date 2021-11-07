c821e4adf40da78d1178e4da100f26aeb3df5384
ThomasDev82 <82886071+ThomasDev82@users.noreply.github.com>
2021-11-07 06:31:01 +0200

    Update login_screen.dart

--- login_screen/lib/login_screen.dart
+++ login_screen/lib/login_screen.dart
@@ -14,83 +14,86 @@ class LoginScreen extends StatelessWidget
   {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children:
-            [
-              const SizedBox(height: 20),
-              const Text(
-                'Login',
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 40),
-              TextFormField(
-                controller: emailControler,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  labelText: 'Email Address',
-                  prefixIcon: Icon(Icons.alternate_email_outlined),
-                  hintText: 'Type your email address',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              const SizedBox(height: 20),
-              TextFormField(
-                controller: passwordControler,
-                keyboardType: TextInputType.visiblePassword,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                  prefixIcon: Icon(Icons.password_outlined),
-                  suffixIcon: Icon(Icons.remove_red_eye_outlined),
-                  hintText: 'Type your password',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              const SizedBox(height: 35),
-              Container(
-                width: double.infinity,
-                color: Colors.blue,
-                child: MaterialButton(
-                  onPressed: ()
-                  {
-                    print(emailControler.text);
-                    print(passwordControler.text);
-                  },
-                  child: const Text(
-                    'LOGIN',
-                    style: TextStyle(
-                      fontSize: 25,
-                      color: Colors.white,
-                    ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children:
+              [
+                const SizedBox(height: 20),
+                const Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children:
-                [
-                  const Text(
-                    'Don\'t have an account?',
-                    style: TextStyle(
-                      fontSize: 15,
-                    ),
+                const SizedBox(height: 40),
+                TextFormField(
+                  controller: emailControler,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    labelText: 'Email Address',
+                    prefixIcon: Icon(Icons.alternate_email_outlined),
+                    hintText: 'Type your email address',
+                    border: OutlineInputBorder(),
                   ),
-                  TextButton(
-                    onPressed: () {},
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  controller: passwordControler,
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    prefixIcon: Icon(Icons.lock),
+                    suffixIcon: Icon(Icons.remove_red_eye_outlined),
+                    hintText: 'Type your password',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 35),
+                Container(
+                  width: double.infinity,
+                  color: Colors.blue,
+                  child: MaterialButton(
+                    onPressed: ()
+                    {
+                      print(emailControler.text);
+                      print(passwordControler.text);
+                    },
                     child: const Text(
-                      'Register Here',
+                      'LOGIN',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                      ),
                     ),
-                  )
-                ],
-              ),
-            ],
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children:
+                  [
+                    const Text(
+                      'Don\'t have an account?',
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Register Here',
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
